f26e7db684eb421f23e5616913077ba580801a58
sanmathi-g <71252983+sanmathi-g@users.noreply.github.com>
2020-11-25 14:27:34 +0530

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import programs
-
-
+from .models import programs,ProgCategory,ProgSeries
 
 class programsSerializer(serializers.ModelSerializer):
     class Meta:
         model = programs
-        fields = ['id', 'title']
+        fields = ['id', 'title','content','prog_series','prog_slug']
+        depth = 1
+
+class ProgCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgCategory
+        fields = ['id','Category_title', 'Category_summary','Category_slug']
+
+class ProgSeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgSeries
+        fields = ['id','prog_series', 'Category_title']
+        depth = 1
